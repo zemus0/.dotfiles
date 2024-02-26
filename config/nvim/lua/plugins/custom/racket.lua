54b030037c7a3ca12d0724a11e13a032155bbe46
zemus0 <62078528+zemus0@users.noreply.github.com>
2024-02-26 14:45:55 -0500

    add racket test

--- config/nvim/lua/plugins/custom/racket.lua
+++ config/nvim/lua/plugins/custom/racket.lua
@@ -1,8 +1,12 @@
 require 'lspconfig'.racket_langserver.setup {}
 
 vim.keymap.set("n", "<leader>rr", function()
-	vim.fn.system('tmux send-keys -t {last} \',exit\' Enter')
-	vim.fn.system('tmux send-keys -t {last} C-l')
+	vim.fn.system('\\tmux send-keys -t {last} \',exit\' Enter')
+	vim.fn.system('\\tmux send-keys -t {last} C-l')
 	vim.fn.system("\\tmux send-keys -t {last} $'racket -i -e \\'(enter! (file \"" ..
 		vim.fn.expand('%:p') .. "\"))\\'' Enter")
 end, { desc = "[R]un [R]acket File" })
+
+vim.keymap.set("n", "<leader>rt", function()
+	vim.fn.system("\\tmux send-keys -t {last} 'raco test \"" .. vim.fn.expand('%:p') .. "\"' Enter")
+end, { desc = "[R]acket [T]est File" })
