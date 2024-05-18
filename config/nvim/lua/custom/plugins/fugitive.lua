a7e6e853524216915e33665219b3c0f4003361bc
zemaus <62078528+zemaus@users.noreply.github.com>
2024-05-18 18:48:36 -0400

    remove keybind for fugitive gitdiff

--- config/nvim/lua/custom/plugins/fugitive.lua
+++ config/nvim/lua/custom/plugins/fugitive.lua
@@ -1,7 +1,3 @@
 return {
   'tpope/vim-fugitive',
-  config = function()
-    vim.keymap.set('n', '<leader>Dh', ':diffget //2', { desc = 'Accept code from the left buffer' })
-    vim.keymap.set('n', '<leader>Dl', ':diffget //3', { desc = 'Accept code from the right buffer' })
-  end,
 }
