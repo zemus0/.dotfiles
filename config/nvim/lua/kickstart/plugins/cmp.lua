502e301f9c2f6ee71669ccb0bcf7a280dad99815
zemaus <62078528+zemaus@users.noreply.github.com>
2024-05-18 20:26:33 -0400

    move away from super tab

--- config/nvim/lua/kickstart/plugins/cmp.lua
+++ config/nvim/lua/kickstart/plugins/cmp.lua
@@ -86,6 +86,22 @@ return {
           -- Select the [p]revious item
           ['<C-p>'] = cmp.mapping.select_prev_item(),
 
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+
           ['<C-u>'] = cmp.mapping.scroll_docs(-4),
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
@@ -98,24 +114,16 @@ return {
               cmp.complete()
             end
           end),
-          ['<Tab>'] = cmp.mapping(function(fallback)
+
+          ['<C-A-Space>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
+              cmp.confirm {
+                behavior = cmp.ConfirmBehavior.Insert,
+              }
             else
               fallback()
             end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
+          end),
         },
         sources = {
           { name = 'nvim_lsp' },
