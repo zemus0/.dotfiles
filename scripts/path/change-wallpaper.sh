d5b29aac23e29159f6a5f53a101bd1d14bfd0ed9
zemus0 <62078528+zemus0@users.noreply.github.com>
2024-09-13 10:25:28 -0400

    fix scripts to run as intended

--- scripts/path/change-wallpaper.sh
+++ scripts/path/change-wallpaper.sh
@@ -81,12 +81,13 @@ if ffprobe "$input" -hide_banner -loglevel panic -select_streams v:0 -show_entri
 fi
 
 wallust run $wallpaper_png
-xrdb ~/.cache/wallust/colors.Xresources
 
 if [ $? != 0 ]; then
-    echo "wallust run command failed, exiting..." 2
+    echo "wallust run command failed, exiting..." >&2
     exit
 fi
+
+xrdb ~/.cache/wallust/colors.Xresources
 
 if [ "$is_vidya" = true ] ; then
     video_resolution=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$input")
