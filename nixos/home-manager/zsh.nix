51d9c2f340676212d70134de750432813d0a7560
zemus0 <62078528+zemus0@users.noreply.github.com>
2024-09-23 11:00:10 -0400

    [direnv] add direnv config & use nix config for direnv

--- nixos/home-manager/zsh.nix
+++ nixos/home-manager/zsh.nix
@@ -2,25 +2,36 @@ let
 	zshrc = builtins.readFile ~/.dotfiles/zsh/zshrc; 
 in
 { config, pkgs, lib, ... }: {
-	programs.zsh = {
-		enable = true;
-		defaultKeymap = "viins";
-		autocd = true;
-		history = {
-			append = true;
-			extended = true;
-			save = 100000;
-			ignoreDups = true;
-			expireDuplicatesFirst = true;
-		};
-		historySubstringSearch.enable = true;
-		syntaxHighlighting.enable = true;
-		antidote = {
+	programs = {
+		direnv = {
 			enable = true;
-			plugins = [
-				"romkatv/powerlevel10k"
-				"joshskidmore/zsh-fzf-history-search"
-				"Aloxaf/fzf-tab"
+			enableZshIntegration = true;
+			nix-direnv.enable = true;
+		};
+
+		zsh = {
+			enable = true;
+			autocd = true;
+			history = {
+				append = true;
+				extended = true;
+				save = 100000;
+				ignoreDups = true;
+				expireDuplicatesFirst = true;
+			};
+			historySubstringSearch.enable = true;
+			syntaxHighlighting.enable = true;
+			antidote = {
+				enable = true;
+				plugins = [
+					"romkatv/powerlevel10k"
+					"jeffreytse/zsh-vi-mode"
+					"oz/safe-paste"
+				];
+			};
+			initExtra = lib.strings.concatStrings[
+				zshrc
+				"\nsource ~/.dotfiles/zsh/p10k.zsh"
 			];
 		};
 		initExtraBeforeCompInit = "zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'";
