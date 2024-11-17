3b4e60086cedc2f90d026f5dd2ab6b0ab25a3d4c
zemus0 <62078528+zemus0@users.noreply.github.com>
2024-11-16 23:11:28 -0500

    Configs: Laptop specific numbers

--- startup.sh
+++ startup.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 # don't need to remap with this keyboard (it have hardware key assignment)
 
-#xbindkeys &
-#xkbset m &
-#xset -r 66 &
-#xset -dpms;xset s off
-
 picom -b
-xrandr --output HDMI-0 --mode 3440x1440 --rate 100
 ~/.dotfiles/scripts/path/change-wallpaper.sh -R
 electron-mail
 dunst
