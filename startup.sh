a479d8c4aeabd7f59c00b05847a146e94e6125e2
zemus0 <62078528+zemus0@users.noreply.github.com>
2024-11-18 18:42:26 -0500

    run all startup command in parallel

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # don't need to remap with this keyboard (it have hardware key assignment)
 
-picom -b
-~/.dotfiles/scripts/path/change-wallpaper.sh -R
-xrandr --output HDMI-0 --mode 3440x1440 --rate 100
-electron-mail
-dunst
-tmux new-session -d
-amixer -c 2 sset 'Auto-Mute Mode' Disabled
+picom -b &
+~/.dotfiles/scripts/path/change-wallpaper.sh -R &
+xrandr --output HDMI-0 --mode 3440x1440 --rate 100 &
+electron-mail &
+dunst &
+tmux new-session -d &
+amixer -c 2 sset 'Auto-Mute Mode' Disabled &
