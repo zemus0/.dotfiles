76514a158aee712d95c29ae713f8b3f8082d3fac
zemus0 <62078528+zemus0@users.noreply.github.com>
2024-11-22 21:05:17 -0500

    fix amixer setting

--- startup.sh
+++ startup.sh
@@ -7,4 +7,4 @@ xrandr --output HDMI-0 --mode 3440x1440 --rate 100 &
 electron-mail &
 dunst &
 tmux new-session -d &
-amixer -c 2 sset 'Auto-Mute Mode' Disabled &
+amixer -c 1 sset 'Auto-Mute Mode' Disabled &
